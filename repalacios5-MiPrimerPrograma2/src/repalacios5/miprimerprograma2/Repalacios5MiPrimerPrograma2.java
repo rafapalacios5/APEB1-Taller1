3c8c1875887a273d3a61c940dd2cf40ded5d4ef2
Rafaella Palacios <repalacios5@utpl.edu.ec>
2023-10-22 18:43:06 -0500

    mi primer programa

---
+++ repalacios5-MiPrimerPrograma2/src/repalacios5/miprimerprograma2/Repalacios5MiPrimerPrograma2.java
@@ -0,0 +1,10 @@
+
+package repalacios5.miprimerprograma2;
+
+public class Repalacios5MiPrimerPrograma2 {
+
+  public static void main(String[] args) {
+  System.out.println("Hola Mundo Rafaella Palacios");
+    }
+    
+}
