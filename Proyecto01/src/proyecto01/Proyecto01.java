8ad8853239ed443492ca0a93fb095485a5797f4a
Rafaella Palacios <repalacios5@utpl.edu.ec>
2023-10-24 05:06:27 -0500

    segundo commit

--- Proyecto01/src/proyecto01/Proyecto01.java
+++ Proyecto01/src/proyecto01/Proyecto01.java
@@ -6,7 +6,7 @@ public class Proyecto01 {
 
    
   public static void main(String[] args) {
-      System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+      System.out.println("Ejemplo de Java Manejado desde GitHub Versionado a través de Git Creado por rafapalacios5");
       
     }
     
