ba6977d8b76efebcb4b44c21975332e5c549124c
Rafaella Palacios <repalacios5@utpl.edu.ec>
2023-10-24 05:19:27 -0500

    tercer commit,trabajado con Git y GitHubClassroom

---
+++ Proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,13 @@
+
+package proyecto02;
+
+
+public class Proyecto02 {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos básico de Java Versionado a través de Git Manejado desde GitHub Además se usar GitHubClassroom");
+       
+    }
+    
+}
